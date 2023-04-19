93e544b6a4e03c9d6407165cfa8f8246c53497db
Dev Kumar Gupta <77541795+DevGupta-ikr@users.noreply.github.com>
2023-04-19 13:24:44 +0530

    Adding relevant comments in code

--- Group Chatting Application/src/group/chatting/application/Server.java
+++ Group Chatting Application/src/group/chatting/application/Server.java
@@ -1,54 +1,80 @@
+/**
+This class represents the Server of the Group Chatting Application.
+*/
+
 package group.chatting.application;
 
 import java.net.*;
 import java.io.*;
 import java.util.*;
 
-public class Server implements Runnable {
-    
+public class Server implements Runnable 
+{
+
     Socket socket;
     
+    // Vector to store all the clients connected to the server
     public static Vector client = new Vector();
     
-    public Server (Socket socket) {
-        try {
+    // Constructor for Server class
+    public Server (Socket socket) 
+    {
+        try 
+        {
             this.socket = socket;
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
     }
     
-    public void run() {
-        try {
+    public void run() 
+    {
+        // Creating input and output streams to communicate with clients
+        try 
+        {
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             
+            // Adding client's output stream to the vector of output streams
             client.add(writer);
             
-            while(true) {
+            // Continuously read messages from the client and send them to all other clients
+            while(true) 
+            {
+                // Reading the message sent by the client
                 String data = reader.readLine().trim();
                 System.out.println("Received " + data);
                 
-                for (int i = 0; i < client.size(); i++) {
-                    try {
+                // Sending the message to all other clients connected to the server
+                for (int i = 0; i < client.size(); i++) 
+                {
+                    try 
+                    {
                         BufferedWriter bw = (BufferedWriter) client.get(i);
                         bw.write(data);
                         bw.write("\r\n");
                         bw.flush();
-                    } catch (Exception e) {
+                    } 
+                    catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } 
+        catch (Exception e) {
+            e.printStackTrace(); // If there is an error, print the stack trace for debugging purposes
         }
     }
 
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception 
+    {
+        // Creating a server socket to accept incoming client connections
         ServerSocket s = new ServerSocket(2003);
-        while(true) {
+        while(true) 
+        {
+            // Accepting client connection requests and starting a new thread to handle each client
             Socket socket = s.accept();
             Server server = new Server(socket);
             Thread thread = new Thread(server);
